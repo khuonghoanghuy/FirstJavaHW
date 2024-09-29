b7cf4746861249813aff606759c4701dea5b0153
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-09-29 16:16:15 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!\nThis is the First Code of my java skill :))");
+    }
+}
